3bbe00a488551bc1c0bca08c1402540e46aca851
alecrj <99alecrodriguez@gmail.com>
2025-07-26 15:20:52 -0400

    Initial Commit

---
+++ ResellAI/ResellAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResellAIApp.swift
+//  ResellAI
+//
+//  Created by Alec on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ResellAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResellAITests/ResellAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ResellAITests.swift
+//  ResellAITests
+//
+//  Created by Alec on 7/26/25.
+//
+
+import Testing
+@testable import ResellAI
+
+struct ResellAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
